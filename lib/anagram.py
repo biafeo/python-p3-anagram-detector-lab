87ec8419bf3eb87d65efb27f436ff5ccbcb9ce76
beatriz <beatrizffeo@gmail.com>
2024-04-24 11:28:13 -0600

    lab complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,17 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, word_list):
+        results = []
+        for word in word_list:
+            if self.is_anagram(word):
+                results.append(word)
+        return results
+
+    def is_anagram(self, other_word):
+        word_list = sorted(list(self.word))
+        other_word_list = sorted(list(other_word))
+        return word_list == other_word_list
